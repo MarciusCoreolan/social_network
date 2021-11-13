53d14fe35aa3e4a5613b9875af3901a0a69617ed
linkoln <79755659+linkoln-1@users.noreply.github.com>
2021-11-13 12:52:32 +0300

    modified new and endly commit for me

--- src/Components/Header/LogInOut/index.jsx
+++ src/Components/Header/LogInOut/index.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import s from './logInOut.module.css';
+import s from './LogInOut.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import ButtonNav from '../../Component/ButtonNav';
 import { logOut, windowOpen } from '../../../Redux/Ducks/userAuthReducer';
 import { addNewPostWindowClose } from '../../../Redux/Ducks/postsReducer';
 
 function LogInOut(props) {
- const dispatch = useDispatch();
- const logInLogOut = useSelector((state) => state.user.logInLogOut);
+  const dispatch = useDispatch();
+  const logInLogOut = useSelector((state) => state.user.logInLogOut);
 
- const handleLogIn = () => {
-  dispatch(windowOpen());
- };
- const handleLogOut = () => {
-  dispatch(logOut());
-  dispatch(addNewPostWindowClose());
- };
+  const handleLogIn = () => {
+    dispatch(windowOpen());
+  };
+  const handleLogOut = () => {
+    dispatch(logOut());
+    dispatch(addNewPostWindowClose());
+  };
 
- return (
-   <div className={s.log_in_out}>
-    {logInLogOut ? (
-      <ButtonNav text={'Выйти'} click={handleLogOut} />
-    ) : (
-      <ButtonNav text={'Войти'} click={handleLogIn} />
-    )}
-   </div>
- );
+  return (
+    <div className={s.log_in_out}>
+      {logInLogOut ? (
+        <ButtonNav text={'Выйти'} click={handleLogOut} />
+      ) : (
+        <ButtonNav text={'Войти'} click={handleLogIn} />
+      )}
+    </div>
+  );
 }
 
 export default LogInOut;
--- src/Components/Main/Pages/Feed/NotAppRovedPosts/index.jsx
+++ src/Components/Main/Pages/Feed/NotAppRovedPosts/index.jsx
@@ -1,50 +1,50 @@
 import React from 'react';
-import Posts from '../Posts/Index';
+import Posts from '../Posts/index';
 import { useDispatch, useSelector } from 'react-redux';
 import {
- adminApprovedPost,
- adminDeletePost,
+  adminApprovedPost,
+  adminDeletePost,
 } from '../../../../../Redux/Ducks/postsReducer';
 
 function NotApprovedPosts(props) {
- const dispatch = useDispatch();
- const posts = useSelector((state) =>
-   state.posts.item.filter((post) => post.approved === false),
- ); //получаем посты и сразу фильтруем на неодобренные
- const userName = useSelector((state) => state.user.item.userName); //получаем юзернейм пользователя который авторизовался
- const moderate = useSelector((state) => state.user.item.moderate); //проверяем является ли пользователь админом
+  const dispatch = useDispatch();
+  const posts = useSelector((state) =>
+    state.posts.item.filter((post) => post.approved === false),
+  ); //получаем посты и сразу фильтруем на неодобренные
+  const userName = useSelector((state) => state.user.item.userName); //получаем юзернейм пользователя который авторизовался
+  const moderate = useSelector((state) => state.user.item.moderate); //проверяем является ли пользователь админом
 
- const handleDelete = (id) => {
-  //функция удаления неодобренной поста админом
-  dispatch(adminDeletePost(id)); //передаем айди поста на сервер
- };
- const handleApproved = (id) => {
-  //функция одобреня поста админом
-  dispatch(adminApprovedPost(id)); //передаем айди поста на сервер
- };
+  const handleDelete = (id) => {
+    //функция удаления неодобренной поста админом
+    dispatch(adminDeletePost(id)); //передаем айди поста на сервер
+  };
+  const handleApproved = (id) => {
+    //функция одобреня поста админом
+    dispatch(adminApprovedPost(id)); //передаем айди поста на сервер
+  };
 
- return (
-   <div>
-    {posts // ниже делаем проверку по полученным ключам зарегистрированы ли
-      // пользователи является ли он админом или автором не одобренного поста
-      .filter((post) => userName === post.author || moderate === true)
-      .map(
-        (
-          post, //если авторизованный пользователь админ или является автором поста то он видит свои не одобренные посты
-        ) => (
-          <Posts
-            post={post}
-            key={post.id}
-            moderate={moderate}
-            approved={post.approved}
-            id={post.id}
-            handleDelete={handleDelete}
-            handleApproved={handleApproved}
-          />
-        ),
-      )}
-   </div>
- );
+  return (
+    <div>
+      {posts // ниже делаем проверку по полученным ключам зарегистрированы ли
+        // пользователи является ли он админом или автором не одобренного поста
+        .filter((post) => userName === post.author || moderate === true)
+        .map(
+          (
+            post, //если авторизованный пользователь админ или является автором поста то он видит свои не одобренные посты
+          ) => (
+            <Posts
+              post={post}
+              key={post.id}
+              moderate={moderate}
+              approved={post.approved}
+              id={post.id}
+              handleDelete={handleDelete}
+              handleApproved={handleApproved}
+            />
+          ),
+        )}
+    </div>
+  );
 }
 
 export default NotApprovedPosts;
